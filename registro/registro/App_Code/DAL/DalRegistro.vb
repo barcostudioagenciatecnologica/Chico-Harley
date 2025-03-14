3fd9ce037a66cb7edb4e246ac7103ff5938c33b2
AvelinoMadrid <avelino_madrid@barcostudio.com>
2025-03-14 16:28:11 -0600

    se puso la conexion a la DB del AWS

--- registro/registro/App_Code/DAL/DalRegistro.vb
+++ registro/registro/App_Code/DAL/DalRegistro.vb
@@ -3,8 +3,8 @@ Imports System.Data
 
 Public Class DalRegistro
     Implements IDisposable
-    'Private Const Cad As String = "Data Source=chicoharley.cnw60iy08a5z.us-east-2.rds.amazonaws.com;Initial Catalog=adminchhly_ChicoHarley;User ID=admin;Password=chico007"
-    Private Const Cad As String = "Data Source=.;Initial Catalog=adminchhly_ChicoHarley;User ID=sa;Password=admin123"
+    Private Const Cad As String = "Data Source=chicoharley.cjwysgo0c41u.us-east-2.rds.amazonaws.com;Initial Catalog=adminchhly_ChicoHarley;User ID=admin;Password=chicoharley2025"
+    'Private Const Cad As String = "Data Source=.;Initial Catalog=adminchhly_ChicoHarley;User ID=sa;Password=admin123"
 
     Property ErrorMessage() As String
     Private ReadOnly _con As SqlConnection
